3eff1b346b4a545357b730f26fd31c955e76466f
leticiafrR <129856789+leticiafrR@users.noreply.github.com>
2024-06-09 14:16:08 -0300

    creation-sistem-l

--- tp2/src/tp2/core.clj
+++ tp2/src/tp2/core.clj
@@ -1,54 +1,70 @@
 (ns tp2.core
   (:require [clojure.java.io :as io]
-            [clojure.string :as str])
-  )
+            [clojure.string :as str]))
 
 ;Lee archivo y lo deja en seq de strings por linea
 ;ruta es string "ruta/ruta.formato"
-(defn leer_archivo [ruta]
-
-  (with-open [lector (io/reader ruta)]
+(defn read-file [path]
+  (with-open [lector (io/reader path)]
     (doall (line-seq lector))))
 
-;devuelve el string de la ruta
-(defn ruta [nombre]
-  (apply str (concat '("resources/") (conj '() nombre))))
+;devuelve un vector con un string por cada salto de linea en el archivo
+(defn read-file [path]
+  (let [all-file (slurp path)]
+    (str/split-lines all-file)))
 
-;recibe una seq de reglas separadas por regla
-;cada regla viena con un espacio entre su predecedor y sucesor
-(defn crearReglas [reglas mapa]
-  (if (not (empty? reglas))
-    (let [regla (seq (.split (first reglas) " "))]
-      (merge mapa (crearReglas (rest reglas) {(first regla) (second regla)})))
-    mapa))
+;recibe un vector con cada regla como elemento
+;devuelve un mapa con pares "predecesor" "sucesor"
+(defn create-rules [reglas]
+  (as-> reglas
+      (mapcat #(str/split % #"\s+") reglas)
+      (zipmap (take-nth 2 reglas) (take-nth 2 (rest reglas)))))
+;("p s" "p2 s2") =>{"p" "s", "p2" "s2"}
 
-(defn juntarLinea [linea]
-  (apply str linea))
 
-(defn separarLinea [linea]
-  (map str (apply concat (partition 1 linea))))
+;recive un vec con lineas del file
+;reglas es un mapa con pares "predecesor" "sucesor"
+(defn create-sistem-l [lines]
+  {:angulo (Float/parseFloat (nth lines 0))
+   :axioma (nth lines 1)
+   :rules (create-rules (nthrest lines 2))})
 
-(defn transformacionLinea [linea reglas]
-  (juntarLinea (replace reglas (separarLinea linea)))
-  )
+;devuelve un sistema que posee: int angulo,str axioma y mapa reglas ("predecesor" "sucesor", )
+(defn load-sistem-l [input]
+  (->> input
+       (:r ,,,)
+       (str "source/" ,,,)
+       (read-file)
+       (create-sistem-l )))
 
-(defn aplicarIteraciones [pila iteraciones reglas]
-  (if (pos? iteraciones)
-    (aplicarIteraciones (conj pila (transformacionLinea (first pila) reglas )) (dec iteraciones) reglas)
-    pila
-    )
-  )
+
+;recive un str estado del axioma y le aplica las transformaciones
+(defn tranformate-1 [reglas-state]
+  (let [reglas (reglas-state 0)
+        state (reglas-state 1)
+        new-state (->> state
+                       (partition 1 ,,,)
+                       (apply concat ,,,)
+                       (map str ,,,)
+                       (replace reglas ,,,)
+                       (apply str ,,,))]
+    [reglas new-state]))
+
+
+;recive la cantidad de veces y un vector reglas-state
+;aplica la transformación n veces, devuelve el estado final
+(defn tranformate [n  reglas-axioma]
+  (as-> reglas-axioma
+       (nth (iterate tranformate-1 reglas-axioma) n)
+       (reglas-axioma 1)))
+
+(defn create-turttle[])
+
+(defn get-input [args]
+  {:r (nth args 0) :v (nth args 1) :w (nth args 2)})
 
 (defn -main [& args]
-  (if (not (= 3 (count args)))
-    (println "Argumentos invalidos")
-    (let [r (first args) v (first (next args)) w (first (nnext args))]
-      (let [datos (leer_archivo (ruta r))]
-        (let [angulo (Integer/parseInt (first datos)) axioma (first (next datos)) reglas (crearReglas (rest (next datos)) nil ) ]
-          (println angulo)
-          (println axioma)
-          (println (transformacionLinea axioma reglas))
-          (println (aplicarIteraciones (conj '() axioma) (Integer/parseInt v) reglas))
-          (println reglas)))
-      (println v)
-      (println w))))
+  (let [input (get-input args)
+        sistem-l (load-sistem-l input )
+        new-axiom (tranformate (input :v) [(sistem-l :rules) (sistem-l :axioma)] )
+        tortuga (create-turttle)]))
